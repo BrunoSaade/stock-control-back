d1056882284d919810fcf773a328dc67813bd148
BrunoSaade <brsaade@gmail.com>
2024-05-14 13:26:55 -0300

    Fix: Unused func removed

--- main.go
+++ main.go
@@ -80,20 +80,6 @@ func main() {
 	log.Fatal(http.ListenAndServe(":"+config.Port(), corsOpts.Handler(r)))
 }
 
-func accessControlMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS,PUT")
-		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
-
-		if r.Method == "OPTIONS" {
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
-
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Running"))
